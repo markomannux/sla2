400eaec9cb8386becf36b662490a42a58e7af195
markomannux <markomannux@gmail.com>
2012-10-21 14:18:26 +0200

    Modularizzata gestione frase Modificata gestione del routing

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var http = require('http');
 var static = require('node-static');
 var url = require('url');
 var util = require('util');
+var sentence = require('./sentence');
 
 var file = new(static.Server)('public', {
   cache: 3600,
@@ -15,7 +16,7 @@ function route(handle, pathname, req, res) {
   if (typeof handle[pathname] === 'function') {
     handle[pathname](req, res);
   } else {
-    handle['static-file'](req, res);
+    serveFile(req, res);
   }
 };
 
@@ -36,103 +37,12 @@ function serveFile(req, res) {
 };
 
 
-var nouns = [
-{nome:"astante", genere:"M"},
-{nome:"busillis", genere:"M"},
-{nome:"caratura", genere:"F"},
-{nome:"crepuscolo", genere:"M"},
-{nome:"dileggiatore", genere:"M"},
-{nome:"discente", genere:"M"},
-{nome:"discrasia", genere:"F"},
-{nome:"epigono", genere:"M"},
-{nome:"fattispecie", genere:"F"},
-{nome:"ignominia", genere:"F"},
-{nome:"illazione", genere:"F"},
-{nome:"improperio", genere:"M"},
-{nome:"latore", genere:"M"},
-{nome:"pleonasmo", genere:"M"},
-{nome:"ritrosia", genere:"F"},
-{nome:"sinossi", genere:"F"},
-{nome:"vanesio", genere:"M"},
-{nome:"vituperio", genere:"M"},
-  ];
-
-var verbs = [
-"depaupera",
-  "dileggia",
-  "esperisce",
-  "ottempera a",
-  "procrastina",
-  "vitupera",
-  "esautora",
-  ];
-
-var adjectives = [
-{M:"desueto", F:"desueta"},
-{M:"esacerbato", F:"esacerbata"},
-{M:"micragnoso", F:"micragnosa"},
-{M:"pleonastico", F:"pleonastica"},
-{M:"protervo", F:"proterva"},
-{M:"querulo", F:"querula"},
-{M:"sesquipedale", F:"sesquipedale"},
-{M:"taumaturgico", F:"taumaturgica"},
-{M:"turpe", F:"turpe"},
-{M:"vanesio", F:"vanesia"},
-  ];
-
-
 function generateSentence(req, res) {
-  res.write(buildSentence());
+  res.write(sentence.buildSentence());
   res.end();
-}
-
-function buildSentence() {
-  var subject = pickRandom(nouns);
-  var verb = pickRandom(verbs);
-  var obj = pickRandom(nouns);
-  var adjective1 = pickRandom(adjectives);
-  var adjective2 = pickRandom(adjectives);
-  var body = selectArticoloDet(adjective1[subject.genere], subject.genere) + " " + adjective1[subject.genere] + " " + subject.nome + " " + verb + " " + selectArticoloInd(adjective2[obj.genere], obj.genere) + " " + adjective2[obj.genere] + " " + obj.nome;
-  body = body.charAt(0).toUpperCase() + body.slice(1) + ".";
-  return body;
-};
-
-function selectArticoloDet(parola, genere) {
-  if (startsWithVowel(parola)) {
-    return "l'";
-  } else if (genere == "M") {
-    return "il";
-  } else {
-    return "la";
-  }
-};
-
-function selectArticoloInd(parola, genere) {
-  if (startsWithVowel(parola)) {
-    if (genere == "M") {
-      return "un";
-    } else {
-      return "un'";
-    }
-
-  } else if (genere == "M") {
-    return "un";
-  } else {
-    return "una";
-  }
-};
-
-function startsWithVowel(word) {
-  var initial = ['a', 'e', 'i', 'o', 'u'].indexOf(word.charAt(0));
-  return initial > -1;
-};
-
-function pickRandom(list) {
-  return list[Math.floor(Math.random()*list.length)];
 };
 
 var handle = {}
-handle["static-file"] = serveFile;
 handle["/generate-sentence"] = generateSentence;
 
 http.createServer(function (req, res) {
